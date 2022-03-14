a3f87febe9b74f95855ffba70ed0f4ffb55f0f75
Lee dohyeong <alsgudwpxm31@gmail.com>
2022-03-14 18:11:31 +0900

    3항연산자 추가

---
+++ src/dogh/hang.java
@@ -0,0 +1,16 @@
+package dogh;
+
+public class hang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b = false;
+		
+		char a = 'a';
+		
+		b = (a == 'a')? true : false;
+		
+		System.out.println("b : " + b);
+	}
+
+}
